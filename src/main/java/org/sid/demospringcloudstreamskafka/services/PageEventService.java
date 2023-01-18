eaf15f7129932d83f5214802651655d743f935f9
salma <salmasaliloupix@gmail.com>
2023-01-18 13:33:08 +0000

    Video 2 fin

--- src/main/java/org/sid/demospringcloudstreamskafka/services/PageEventService.java
+++ src/main/java/org/sid/demospringcloudstreamskafka/services/PageEventService.java
@@ -1,9 +1,16 @@
 package org.sid.demospringcloudstreamskafka.services;
 
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.KeyValue;
+import org.apache.kafka.streams.kstream.Grouped;
+import org.apache.kafka.streams.kstream.KStream;
+import org.apache.kafka.streams.kstream.Materialized;
+import org.apache.kafka.streams.kstream.TimeWindows;
 import org.sid.demospringcloudstreamskafka.Entities.PageEvent;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
 
+import java.time.Duration;
 import java.util.Date;
 import java.util.Random;
 import java.util.function.Consumer;
@@ -15,9 +22,9 @@ public class PageEventService {
     @Bean
     public Consumer<PageEvent> pageEventConsumer(){
         return (input) -> {
-            System.out.println("*******");
+            System.out.println("*******************");
             System.out.println(input.toString());
-            System.out.println("*******");
+            System.out.println("*******************");
         };
     }
 
@@ -37,5 +44,17 @@ public class PageEventService {
             return input;
         };
     }
-
+    @Bean
+    public Function<KStream<String, PageEvent>, KStream<String,Long>> kStreamFunction(){
+        return (input) -> {
+            return  input
+                    .filter((k, v)->v.getDuration()>100)
+                    .map((k,v)->new KeyValue<>(v.getName(), 0L))
+                    .groupBy((k,v)->k, Grouped.with(Serdes.String(), Serdes.Long()))
+                    .windowedBy(TimeWindows.of(Duration.ofMillis(5000)))
+                    .count(Materialized.as("page-count"))
+                    .toStream()
+                    .map((k,v)->new KeyValue<>("=>"+k.window().startTime()+k.window().endTime()+k.key(),v));
+        };
+    }
 }
